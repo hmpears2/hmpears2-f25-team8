483b9dddf03c88bc0a33189841375a65a43cfef6
hmpears2 <hmpears2@uncg.edu>
2025-10-28 08:49:10 -0400

    Added previous files replaced and commented out review section that needs get reviews by id added to provider.

--- backend-api/src/main/java/com/HomeConnectPro_hub/provider/ProviderRepository.java
+++ backend-api/src/main/java/com/HomeConnectPro_hub/provider/ProviderRepository.java
@@ -1,13 +1,10 @@
 package com.HomeConnectPro_hub.provider;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.HomeConnectPro_hub.provider.Provider;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
     Optional<Provider> findByEmailAndPassword(String email, String password);
     List<Provider> findByUserType(String userType);
-    Optional<Provider> findByEmail(String email);
-    List<Provider> findByActiveTrue();
 }
