4529553360fff5962d5510239bbbd5171e00773a
hmpears2 <hmpears2@uncg.edu>
2025-11-19 15:52:03 -0500

    added the ability to update email as well.

--- front-end/src/types/types.ts
+++ front-end/src/types/types.ts
@@ -84,6 +84,7 @@ export interface CustomerRegistrationRequest {
 export interface UpdateCustomerRequest {
   firstName?: string;
   lastName?: string;
+  email?: string;
   phoneNumber?: string;
   address?: string;
   password?: string;
